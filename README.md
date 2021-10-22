8a7c171bb844c85f1f7b53652d02639cfda02888
HobbZ <zachobba@gmail.com>
2021-10-22 13:35:32 +1000

    got most layout working, content table isn't laying out properly for some reason

--- README.md
+++ README.md
@@ -2,34 +2,39 @@
 # README-Generator
 
 ## Description
-Generate Readme via prompts
+<h5>Project Aim</h5>The aim of this project was to create a README generator as per the specifications of my homework assignment.<h5>What Problem Does It Solve</h5>The generator enables me to produce faster and more efficient README documentation<h5>What I Learnt</h5>in making this project, I learnt how to use Node.js, string literals, inquirer and validating inputs
 
 ## Table of Contents
-- [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[Technology](#technology)
+[Contributors](#contributors)
+[Aknowledgements](#acknowledgements)
+[Testing](#testing)
+[License](#license)
+[Questions](#questions)
 
 ## Installation
-Node.js, npm, inquirer
+undefined
 
 ## Usage
-Clone to your computer, open in vs code and open terminal, type node index.js and answer prompts
+Download and install node.js
 
-## Features
-inquirer
-
-//Link github to 
+## Technology
+JavaScript<br>Node.js<br>npm<br>inquirer
+ 
 ## Contributors
 [Zachary Hobba](https://github.com/HobbaZ)
 
 ## Aknowledgements
-Sheilds.io
-
-## License
-![License](https://img.shields.io/badge/License-MIT-blue.svg)
+https://www.npmjs.com/package/inquirer#questions<br><br>https://choosealicense.com/<br><br>https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide<br><br>https://github.com/SBoudrias/Inquirer.js
 
 ## Testing
-None
+npm tests
+
+## License
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://choosealicense.com/licenses/mit/)
 
 ## Questions
 Find me on Github at [HobbaZ](https://github.com/HobbaZ)
--- index.js
+++ index.js
@@ -8,41 +8,85 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 const promptUser = () => {
   return inquirer.prompt([
+
     {
     name: 'title',
     type: 'input',
-    message: 'What is the project title?',
+    message: 'Enter a project title (Hint: same as Github Repo)',
+    default: 'README-Generator',
+    validate: function(title) {
+			if (title) {
+				return true;
+			} else {
+        return 'Please create a title for your README.';
+      }
+		},
+
   },
     
 
-    {
-    name: 'description',
-    type: 'input',
-    message: 'Write a description of the project',
-    },
+  {
+  name: 'description',
+  type: 'input',
+  message: 'Write a description of the project',
+  default: '<h5>Project Aim</h5>The aim of this project was to create a README generator as per the specifications of my homework assignment.<h5>What Problem Does It Solve</h5>The generator enables me to produce faster and more efficient README documentation<h5>What I Learnt</h5>in making this project, I learnt how to use Node.js, string literals, inquirer and validating inputs'
+  ,
+  validate: function(description) {
+		if (description) {
+			return true;
+		} else {
+      return 'Please create a description for your project README.';
+    }
+	}
+  },
 
     {
-    name: 'install',
+    name: 'installation',
     type: 'input',
     message: 'What requirements are needed to run this project?',
+    validate: function(install) {
+			if (install) {
+				return true;
+			} else {
+        return 'Please enter the install process of your project.';
+      }
+		}
     },
 
     {
     name: 'usage',
     type: 'input',
     message: 'How do you use the project?',
+    validate: function(usage) {
+			if (usage) {
+				return true;
+			} else {
+        return 'Please create step by step how to use process for your project';
+      }
+		}
     },
 
     {
-    name: 'features',
+    name: 'technology',
     type: 'input',
-    message: 'List the project features e.g. frameworks used, programming languages used',
+    message: 'List the project features e.g. frameworks used, programming languages used with comma seperator E.g. HTML,CSS',
+    //Fill out technology used here with comma separarators
+    default: 'JavaScript,Node.js,npm,inquirer',
+    validate: function(features) {
+			if (features) {
+				return true;
+			} else {
+        return 'Please enter the frameworks used.';
+      }
+		}
     },
 
+    //Replace default name with your name
     {
-    name: 'contribute',
+    name: 'contributors',
     type: 'input',
-    message: 'Who contributed to the project?',
+    message: 'Who contributed to the project and how can others contribute? (You are also a contributor)',
+    default: 'Zachary Hobba',
     },
 
     {
@@ -50,12 +94,15 @@ const promptUser = () => {
     type: 'input',
     message: 'What other assets did you use to help create this project? E.g. Used font from Google Fonts, \
     Bootstrap for layout',
+    //input full addresses here separated by come
+    default: 'https://www.npmjs.com/package/inquirer#questions,https://choosealicense.com/,https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide,https://github.com/SBoudrias/Inquirer.js'
     },   
 
     {
     name: 'testing',
     type: 'input',
     message: 'What tests can be performed on the project?',
+    default: 'npm tests',
     },
 
     {
@@ -69,26 +116,72 @@ const promptUser = () => {
     name: 'github',
     type: 'input',
     message: 'What is your Github username?',
+    default: 'HobbaZ'
     },
 
     {
     name: 'email',
     type: 'input',
     message: 'What is your email address?',
-    },
-]);
+    default: 'zachobba@gmail.com',
+    validate: function(email) {
+			let test = email.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+			if (test) {
+				return true;
+			} else {
+        return 'Please enter a valid email.';
+      }
+		}
+  },
+
+  ]);
 };
 
+function genTechnology(answers) {
+  let format = '';
+  if (answers.technology) {
+    format = answers.technology.split(",").join("<br>");
+  } else {
+    format = '';
+  }
+  return format;
+}
+
 function genBadge(answers) {
-  const licenseType = answers.license;
   let licenseStr = '';
   if (answers.license) {
-    licenseStr = `![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)`
+    const licenseLink = answers.license.toLowerCase();
+    licenseStr = `[![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)](https://choosealicense.com/licenses/${licenseLink}/)`
   } else {
     licenseStr = '';
   }
   return licenseStr;
 }
+
+function genAcknowledgements(answers) {
+  let format = '';
+  if (answers.acknowledgements) {
+    format = answers.acknowledgements.split(",").join("<br><br>");
+  } else {
+    format = '';
+  }
+  return format;
+}
+
+/*function genTableofContents(answers) {
+
+  answeredPrompts =[]
+
+  if(answers.length !== 0) {
+    answeredPrompts.push(Object.getOwnPropertyNames(answers));
+  }
+    let toc = '##Table of Contents';
+
+    for (let index = 0; index < answeredPrompts.length; index++) {
+      toc += `\n[${answeredPrompts[index]}](#${answeredPrompts[index]})\n`;
+    }
+    return toc;
+}*/
 
 const generateREADME = (answers) => 
 
@@ -99,9 +192,15 @@ const generateREADME = (answers) =>
 ${answers.description}
 
 ## Table of Contents
-- [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
+[Description](#description)
+[Installation](#installation)
+[Usage](#usage)
+[Technology](#technology)
+[Contributors](#contributors)
+[Aknowledgements](#acknowledgements)
+[Testing](#testing)
+[License](#license)
+[Questions](#questions)
 
 ## Installation
 ${answers.install}
@@ -109,21 +208,20 @@ ${answers.install}
 ## Usage
 ${answers.usage}
 
-## Features
-${answers.features}
-
-//Link github to 
+## Technology
+${genTechnology(answers)}
+ 
 ## Contributors
-[${answers.contribute}](https://github.com/HobbaZ)
+[${answers.contributors}](https://github.com/HobbaZ)
 
 ## Aknowledgements
-${answers.acknowledgements}
-
-## License
-${genBadge(answers)}
+${genAcknowledgements(answers)}
 
 ## Testing
 ${answers.testing}
+
+## License
+${genBadge(answers)}
 
 ## Questions
 Find me on Github at [${answers.github}](https://github.com/${answers.github})
@@ -132,6 +230,8 @@ Email me at [${answers.email}](${answers.email})
 `;
 
 const init = () => {
+
+  console.log("____________________README GENERATOR______________________\n Follow the prompts to generate a professional README")
     promptUser()
       .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
       .then(() => console.log('Successfully created README.md'))
